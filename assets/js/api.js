328a8809eaff0b75fd9e59ffe413561666eb4d52
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-09-13 12:10:11 -0300

    second commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
-  const url ="https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json";
+  const url =
+    "https://raw.githubusercontent.com/JuanSsilva07/Portfolio/main/data/profile.json?token=GHSAT0AAAAAABWEFIQK5BY6ZG2FUR3SQHZGYZAVBQQ";
   const feching = await fetch(url);
-  return await feching.json()
+  return await feching.json();
 }
